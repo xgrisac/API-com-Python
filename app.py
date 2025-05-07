5d83e786ea534a23431b8623784a1264e37e9a5a
Isac Ribeiro <isackolling122@gmail.com>
2025-05-07 20:19:17 -0300

    feat: inclusão de endpoint de criação e exclusão
    Métodos HTTP utilizados: POST e DELETE

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import Flask, jsonify, request # jsonify é o formato de retorno da A
 app = Flask(__name__)
 
 # Lista de dicionários
+
 livros = [ 
     {
         'id' : 1,
@@ -29,28 +30,41 @@ livros = [
     },
 ]
 
-# Requisição consultar todos
+# Endpoint para consultar todos
 @app.route('/livros', methods=['GET']) # Detalhe que torna minha função um endpoint da API
 def obter_livros():
     return jsonify(livros)
 
-# Requisição para consultar por ID
-@app.route('/livros/<int:id>', methods=['GET'])
+# Endpoint para consultar por ID
+@app.route('/livros/<int:id>', methods=['GET']) # INT define que o valor que precisa retornar é inteiro e correspondente ao meu ID
 def obter_livros_por_id(id):
     for livro in livros:
         if livro.get('id') == id: # Verifica se o ID atual é igual ao solicitado
             return jsonify(livro)
 
-# Requisição para edição
-@app.route('/livros/<int>:id', methods=['PUT'])
+# Endpoint para edição por ID
+@app.route('/livros/<int:id>', methods=['PUT'])
 def editar_livro_por_id(id):
-    livro_alterado = request.get_json() # Retorna as informações do usuário para a API
+    livro_alterado = request.get_json() # Envia as informações do usuário para a API
     for indice,livro in enumerate(livros):
         if livro.get('id') == id:
             livros[indice].update(livro_alterado)
-            return jsonify(livro[indice])
+            return jsonify(livros[indice])
         
+# Endpoint para criação
+@app.route('/livros', methods=['POST'])
+def criar_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro) #Append adciona 
+    return jsonify(livros)
 
+# Endpoint para exclusão
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def deletar_livro(id):
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice] # Delete de acordo com o respectivo índice
+            return jsonify(livros)
 
 app.run(port=5000, host='localhost', debug=True)
 
