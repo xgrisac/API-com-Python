78140ac9b4a5f7dbe1dc709ce4bc731a29a01371
Isac Ribeiro <isackolling122@gmail.com>
2025-05-08 19:54:22 -0300

    refactor: Alteração na proposta da API
    A nova proposta é entregar uma API para registro de veículos em um estacionamento local.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# API - Local que disponibiliza recursos/funcionalidades
-# 1 - OBJETIVO - Criar uma API que disponibiliza consulta, criação, edição e exclusão de livros
+# API - Local que disponibiliza recursos/funcionalplacaades
+# 1 - OBJETIVO - Criar uma API que disponibiliza consulta, criação, edição e exclusão de carros
 # 2 - URL BASE - localhost.com
 # 3 - ENDPOINT - GET, POST, PUT E DELETE
-# 4 - RECURSOS - Livros
+# 4 - RECURSOS - carros
 
 from flask import Flask, jsonify, request # jsonify é o formato de retorno da API e request acesso os dados que estão indo e vindo da requisção
 
@@ -10,62 +10,62 @@ app = Flask(__name__)
 
 # Lista de dicionários
 
-livros = [ 
+carros = [ 
     {
-        'id' : 1,
-        'titulo' : 'O Senhor dos Anéis - A Sociedade do Anel',
-        'autor' : 'J.R.R Tolkien'
+        'placa' : 'GXG7B73',
+        'Modelo' : 'Fiat Palio',
+        'Condutor' : 'Fulano de tal'
     },
 
     {
-        'id' : 2,
-        'titulo' : 'Livro 2',
-        'autor' : 'Autor 2'
+        'placa' : 'ABC1D23',
+        'Modelo' : 'Volkswagem Gol',
+        'Condutor' : 'Ciclano da Silva'
     },
 
     {
-        'id' : 3,
-        'titulo' : 'Livro 3',
-        'autor' : 'Autor 3'
+        'placa' : 'XYZ9K88',
+        'Modelo' : 'Honda Xre',
+        'Condutor' : 'Beltrano de Souza'
     },
 ]
 
 # Endpoint para consultar todos
-@app.route('/livros', methods=['GET']) # Detalhe que torna minha função um endpoint da API
-def obter_livros():
-    return jsonify(livros)
+@app.route('/carros', methods=['GET']) # Detalhe que torna minha função um endpoint da API
+def obter_carros():
+    return jsonify(carros)
 
-# Endpoint para consultar por ID
-@app.route('/livros/<int:id>', methods=['GET']) # INT define que o valor que precisa retornar é inteiro e correspondente ao meu ID
-def obter_livros_por_id(id):
-    for livro in livros:
-        if livro.get('id') == id: # Verifica se o ID atual é igual ao solicitado
-            return jsonify(livro)
+# Endpoint para consultar por placa
+@app.route('/carros/<placa>', methods=['GET']) # INT define que o valor que precisa retornar é inteiro e correspondente ao meu placa
+def obter_carros_por_placa(placa):
+    for carro in carros:
+        if carro.get('placa') == placa: # Verifica se o placa atual é igual ao solicitado
+            return jsonify(carro)
 
-# Endpoint para edição por ID
-@app.route('/livros/<int:id>', methods=['PUT'])
-def editar_livro_por_id(id):
-    livro_alterado = request.get_json() # Envia as informações do usuário para a API
-    for indice,livro in enumerate(livros):
-        if livro.get('id') == id:
-            livros[indice].update(livro_alterado)
-            return jsonify(livros[indice])
+# Endpoint para edição por placa
+@app.route('/carros/<placa>', methods=['PUT'])
+def editar_carro_por_placa(placa):
+    carro_alterado = request.get_json() # Envia as informações do usuário para a API
+    for indice,carro in enumerate(carros):
+        if carro.get('placa') == placa:
+            carros[indice].update(carro_alterado)
+            return jsonify(carros[indice])
         
 # Endpoint para criação
-@app.route('/livros', methods=['POST'])
-def criar_novo_livro():
-    novo_livro = request.get_json()
-    livros.append(novo_livro) #Append adciona 
-    return jsonify(livros)
+@app.route('/carros', methods=['POST'])
+def criar_novo_carro():
+    novo_carro = request.get_json()
+    carros.append(novo_carro) #Append adciona 
+    return jsonify(carros)
 
 # Endpoint para exclusão
-@app.route('/livros/<int:id>', methods=['DELETE'])
-def deletar_livro(id):
-    for indice,livro in enumerate(livros):
-        if livro.get('id') == id:
-            del livros[indice] # Delete de acordo com o respectivo índice
-            return jsonify(livros)
+@app.route('/carros/<placa>', methods=['DELETE'])
+def deletar_carro(placa):
+    for indice,carro in enumerate(carros):
+        if carro.get('placa') == placa:
+            del carros[indice] # Delete de acordo com o respectivo índice
+            return jsonify(carros)
 
 app.run(port=5000, host='localhost', debug=True)
 
-# http://localhost:5000/livros link real de exibição no postman/navegador
+# http://localhost:5000/carros link real de exibição no postman/navegador
